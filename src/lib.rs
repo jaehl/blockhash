f1aaadf3f15f5aeceb6ada21fffe23ced0042637
James Mills <james@jaehl.net>
2020-05-26 18:46:02 +0100

    Add wrapper types for hash digests

--- src/lib.rs
+++ src/lib.rs
@@ -8,15 +8,14 @@
 //! Basic usage:
 //!
 //! ```
-//! # #[cfg(all(feature = "image", feature = "std"))]
-//! # {
-//! use blockhash::blockhash256_string;
+//! # #[cfg(all(feature = "image"))] {
+//! use blockhash::blockhash256;
 //!
 //! let img = image::open("tests/images/512x512_rgb.png").unwrap();
-//! let hash = blockhash256_string(&img);
+//! let hash = blockhash256(&img);
 //!
 //! assert_eq!(
-//!     hash,
+//!     hash.to_string(),
 //!     "9cfde03dc4198467ad671d171c071c5b1ff81bf919d9181838f8f890f807ff01",
 //! );
 //! # }
@@ -32,6 +31,9 @@
 
 #![cfg_attr(not(feature = "std"), no_std)]
 #![cfg_attr(docsrs, feature(doc_cfg))]
+
+use core::fmt::{self, Display, Formatter};
+use core::str::FromStr;
 
 /// Provides access to image data.
 pub trait Image {
@@ -59,6 +61,19 @@ where
     }
 }
 
+#[derive(Debug, Copy, Clone, PartialEq, Eq)]
+pub struct BlockhashParseError;
+
+impl Display for BlockhashParseError {
+    #[inline(always)]
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        f.write_str("invalid hash string")
+    }
+}
+
+#[cfg(feature = "std")]
+impl std::error::Error for BlockhashParseError {}
+
 #[inline(always)]
 #[allow(clippy::many_single_char_names)]
 fn get_value<I: Image>(img: &I, x: u32, y: u32) -> u64 {
@@ -66,6 +81,16 @@ fn get_value<I: Image>(img: &I, x: u32, y: u32) -> u64 {
         [.., 0] => u64::from(u8::MAX) * 3,
         [r, g, b, _] => u64::from(r) + u64::from(g) + u64::from(b),
     }
+}
+
+fn parse_char(c: u8) -> Result<u8, BlockhashParseError> {
+    let val = match c {
+        b'0'..=b'9' => c - b'0',
+        b'a'..=b'f' => c - (b'a' - 10),
+        b'A'..=b'F' => c - (b'A' - 10),
+        _ => return Err(BlockhashParseError),
+    };
+    Ok(val)
 }
 
 macro_rules! blockhash_impl {
@@ -91,16 +116,37 @@ macro_rules! blockhash_impl {
                 convert_to_bits(width, height, values)
             }
 
-            #[cfg(feature = "std")]
-            pub(super) fn blockhash_string<I: Image>(img: &I) -> String {
-                use std::fmt::Write;
+            pub(super) fn from_str(s: &str) -> Result<[u8; HASH_BYTES], BlockhashParseError> {
+                let s = s.as_bytes();
 
-                let hash = blockhash(img);
-                let mut res = String::new();
-                for byte in &hash {
-                    write!(res, "{:02x}", byte).unwrap();
+                if s.len() != HASH_BYTES * 2 {
+                    return Err(BlockhashParseError);
                 }
-                res
+
+                let mut bytes = [0; HASH_BYTES];
+
+                for i in 0..HASH_BYTES {
+                    bytes[i] = (parse_char(s[2 * i])? << 4) | parse_char(s[2 * i + 1])?;
+                }
+
+                Ok(bytes)
+            }
+
+            pub(super) fn fmt(f: &mut Formatter, hash: [u8; HASH_BYTES]) -> fmt::Result {
+                for byte in &hash {
+                    write!(f, "{:02x}", byte)?;
+                }
+                Ok(())
+            }
+
+            pub(super) fn distance(left: &[u8; HASH_BYTES], right: &[u8; HASH_BYTES]) -> u32 {
+                let mut dist = 0;
+
+                for i in 0..HASH_BYTES {
+                    dist += (left[i] ^ right[i]).count_ones();
+                }
+
+                dist
             }
 
             fn get_values_aligned<I: Image>(img: &I) -> [u64; HASH_SIZE] {
@@ -319,227 +365,355 @@ blockhash_impl!(impl64, 8);
 blockhash_impl!(impl144, 12);
 blockhash_impl!(impl256, 16);
 
-/// Generates a 16-bit perceptual hash of an image as an array of bytes.
+/// Generates a 16-bit perceptual hash of an image.
 ///
 /// # Examples
 ///
 /// ```
-/// # #[cfg(feature = "image")]
-/// # {
-/// use blockhash::blockhash16;
+/// # #[cfg(feature = "image")] {
+/// use blockhash::{blockhash16, Blockhash16};
 ///
 /// let img = image::open("tests/images/512x512_rgb.png").unwrap();
 /// let hash = blockhash16(&img);
 ///
-/// assert_eq!(&hash[..], &[0x35, 0x6c]);
+/// assert_eq!(hash, Blockhash16::from([0x35, 0x6c]));
 /// # }
 /// ```
 #[inline(always)]
-pub fn blockhash16<I: Image>(img: &I) -> [u8; 2] {
-    impl16::blockhash(img)
+pub fn blockhash16<I: Image>(img: &I) -> Blockhash16 {
+    Blockhash16(impl16::blockhash(img))
 }
 
-/// Generates a 16-bit perceptual hash of an image as an integer.
+/// A 16-bit hash digest.
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Blockhash16([u8; 2]);
+
+impl Blockhash16 {
+    /// Returns the Hamming distance between two hashes.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use blockhash::Blockhash16;
+    ///
+    /// let a = Blockhash16::from([0xff, 0x80]);
+    /// let b = Blockhash16::from([0xf7, 0xc1]);
+    ///
+    /// assert_eq!(a.distance(&b), 3);
+    /// ```
+    #[inline(always)]
+    pub fn distance(&self, other: &Self) -> u32 {
+        impl16::distance(&self.0, &other.0)
+    }
+}
+
+impl FromStr for Blockhash16 {
+    type Err = BlockhashParseError;
+
+    #[inline(always)]
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        impl16::from_str(s).map(Self)
+    }
+}
+
+impl Display for Blockhash16 {
+    #[inline(always)]
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        impl16::fmt(f, self.0)
+    }
+}
+
+impl From<[u8; 2]> for Blockhash16 {
+    #[inline(always)]
+    fn from(bytes: [u8; 2]) -> Self {
+        Blockhash16(bytes)
+    }
+}
+
+impl From<Blockhash16> for [u8; 2] {
+    #[inline(always)]
+    fn from(hash: Blockhash16) -> Self {
+        hash.0
+    }
+}
+
+impl From<u16> for Blockhash16 {
+    #[inline(always)]
+    fn from(int: u16) -> Self {
+        Blockhash16(int.to_be_bytes())
+    }
+}
+
+impl From<Blockhash16> for u16 {
+    #[inline(always)]
+    fn from(hash: Blockhash16) -> Self {
+        u16::from_be_bytes(hash.0)
+    }
+}
+
+/// Generates a 64-bit perceptual hash of an image.
 ///
 /// # Examples
 ///
 /// ```
-/// # #[cfg(feature = "image")]
-/// # {
-/// use blockhash::blockhash16_int;
-///
-/// let img = image::open("tests/images/512x512_rgb.png").unwrap();
-/// let hash = blockhash16_int(&img);
-///
-/// assert_eq!(hash, 0x356c);
-/// # }
-/// ```
-#[inline(always)]
-pub fn blockhash16_int<I: Image>(img: &I) -> u16 {
-    u16::from_be_bytes(blockhash16(img))
-}
-
-/// Generates a 16-bit perceptual hash of an image as a hex string.
-///
-/// # Examples
-///
-/// ```
-/// # #[cfg(all(feature = "image", feature = "std"))]
-/// # {
-/// use blockhash::blockhash16_string;
-///
-/// let img = image::open("tests/images/512x512_rgb.png").unwrap();
-/// let hash = blockhash16_string(&img);
-///
-/// assert_eq!(hash, "356c");
-/// # }
-/// ```
-#[cfg(feature = "std")]
-#[cfg_attr(docsrs, doc(cfg(feature = "std")))]
-#[inline(always)]
-pub fn blockhash16_string<I: Image>(img: &I) -> String {
-    impl16::blockhash_string(img)
-}
-
-/// Generates a 64-bit perceptual hash of an image as an array of bytes.
-///
-/// # Examples
-///
-/// ```
-/// # #[cfg(feature = "image")]
-/// # {
-/// use blockhash::blockhash64;
+/// # #[cfg(feature = "image")] {
+/// use blockhash::{blockhash64, Blockhash64};
 ///
 /// let img = image::open("tests/images/512x512_rgb.png").unwrap();
 /// let hash = blockhash64(&img);
 ///
-/// assert_eq!(&hash[..], &[0xaf, 0x05, 0x75, 0x29, 0x7c, 0x4c, 0x4c, 0xe3]);
+/// assert_eq!(
+///     hash,
+///     Blockhash64::from([0xaf, 0x05, 0x75, 0x29, 0x7c, 0x4c, 0x4c, 0xe3]),
+/// );
 /// # }
 /// ```
 #[inline(always)]
-pub fn blockhash64<I: Image>(img: &I) -> [u8; 8] {
-    impl64::blockhash(img)
+pub fn blockhash64<I: Image>(img: &I) -> Blockhash64 {
+    Blockhash64(impl64::blockhash(img))
 }
 
-/// Generates a 64-bit perceptual hash of an image as an integer.
+/// A 64-bit hash digest.
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Blockhash64([u8; 8]);
+
+impl Blockhash64 {
+    /// Returns the Hamming distance between two hashes.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use blockhash::Blockhash64;
+    ///
+    /// let a = Blockhash64::from([
+    ///     0xde, 0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xef,
+    /// ]);
+    /// let b = Blockhash64::from([
+    ///     0xd0, 0xad, 0xbe, 0xef, 0xde, 0xad, 0xbe, 0xff,
+    /// ]);
+    ///
+    /// assert_eq!(a.distance(&b), 4);
+    /// ```
+    #[inline(always)]
+    pub fn distance(&self, other: &Self) -> u32 {
+        impl64::distance(&self.0, &other.0)
+    }
+}
+
+impl FromStr for Blockhash64 {
+    type Err = BlockhashParseError;
+
+    #[inline(always)]
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        impl64::from_str(s).map(Self)
+    }
+}
+
+impl Display for Blockhash64 {
+    #[inline(always)]
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        impl64::fmt(f, self.0)
+    }
+}
+
+impl From<[u8; 8]> for Blockhash64 {
+    #[inline(always)]
+    fn from(bytes: [u8; 8]) -> Self {
+        Blockhash64(bytes)
+    }
+}
+
+impl From<Blockhash64> for [u8; 8] {
+    #[inline(always)]
+    fn from(hash: Blockhash64) -> Self {
+        hash.0
+    }
+}
+
+impl From<u64> for Blockhash64 {
+    #[inline(always)]
+    fn from(int: u64) -> Self {
+        Blockhash64(int.to_be_bytes())
+    }
+}
+
+impl From<Blockhash64> for u64 {
+    #[inline(always)]
+    fn from(hash: Blockhash64) -> Self {
+        u64::from_be_bytes(hash.0)
+    }
+}
+
+/// Generates a 144-bit perceptual hash of an image.
 ///
 /// # Examples
 ///
 /// ```
-/// # #[cfg(feature = "image")]
-/// # {
-/// use blockhash::blockhash64_int;
-///
-/// let img = image::open("tests/images/512x512_rgb.png").unwrap();
-/// let hash = blockhash64_int(&img);
-///
-/// assert_eq!(hash, 0xaf05_7529_7c4c_4ce3);
-/// # }
-/// ```
-#[inline(always)]
-pub fn blockhash64_int<I: Image>(img: &I) -> u64 {
-    u64::from_be_bytes(blockhash64(img))
-}
-
-/// Generates a 64-bit perceptual hash of an image as a hex string.
-///
-/// # Examples
-///
-/// ```
-/// # #[cfg(all(feature = "image", feature = "std"))]
-/// # {
-/// use blockhash::blockhash64_string;
-///
-/// let img = image::open("tests/images/512x512_rgb.png").unwrap();
-/// let hash = blockhash64_string(&img);
-///
-/// assert_eq!(hash, "af0575297c4c4ce3");
-/// # }
-/// ```
-#[cfg(feature = "std")]
-#[cfg_attr(docsrs, doc(cfg(feature = "std")))]
-#[inline(always)]
-pub fn blockhash64_string<I: Image>(img: &I) -> String {
-    impl64::blockhash_string(img)
-}
-
-/// Generates a 144-bit perceptual hash of an image as an array of bytes.
-///
-/// # Examples
-///
-/// ```
-/// # #[cfg(feature = "image")]
-/// # {
-/// use blockhash::blockhash144;
+/// # #[cfg(feature = "image")] {
+/// use blockhash::{blockhash144, Blockhash144};
 ///
 /// let img = image::open("tests/images/512x512_rgb.png").unwrap();
 /// let hash = blockhash144(&img);
 ///
 /// assert_eq!(
-///     &hash[..],
-///     &[
+///     hash,
+///     Blockhash144::from([
 ///         0x93, 0xfc, 0x0d, 0x91, 0x3b, 0xd3, 0x18, 0x33, 0x2b,
 ///         0x37, 0xc3, 0x7d, 0x30, 0x83, 0x28, 0xe2, 0xef, 0x83,
-///     ],
+///     ]),
 /// );
 /// # }
 /// ```
 #[inline(always)]
-pub fn blockhash144<I: Image>(img: &I) -> [u8; 18] {
-    impl144::blockhash(img)
+pub fn blockhash144<I: Image>(img: &I) -> Blockhash144 {
+    Blockhash144(impl144::blockhash(img))
 }
 
-/// Generates a 144-bit perceptual hash of an image as a hex string.
+/// A 144-bit hash digest.
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Blockhash144([u8; 18]);
+
+impl Blockhash144 {
+    /// Returns the Hamming distance between two hashes.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use blockhash::Blockhash144;
+    ///
+    /// let a = Blockhash144::from([
+    ///     0x00, 0x11, 0x22, 0x33, 0x44, 0x55, 0x66, 0x77, 0x88,
+    ///     0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xee, 0xff, 0x12, 0x34,
+    /// ]);
+    /// let b = Blockhash144::from([
+    ///     0x00, 0x11, 0x22, 0x33, 0x22, 0x55, 0x66, 0x77, 0x88,
+    ///     0x99, 0xaa, 0xbb, 0xcc, 0xdd, 0xe7, 0xff, 0x12, 0x34,
+    /// ]);
+    ///
+    /// assert_eq!(a.distance(&b), 6);
+    /// ```
+    #[inline(always)]
+    pub fn distance(&self, other: &Self) -> u32 {
+        impl144::distance(&self.0, &other.0)
+    }
+}
+
+impl FromStr for Blockhash144 {
+    type Err = BlockhashParseError;
+
+    #[inline(always)]
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        impl144::from_str(s).map(Self)
+    }
+}
+
+impl Display for Blockhash144 {
+    #[inline(always)]
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        impl144::fmt(f, self.0)
+    }
+}
+
+impl From<[u8; 18]> for Blockhash144 {
+    #[inline(always)]
+    fn from(bytes: [u8; 18]) -> Self {
+        Blockhash144(bytes)
+    }
+}
+
+impl From<Blockhash144> for [u8; 18] {
+    #[inline(always)]
+    fn from(hash: Blockhash144) -> Self {
+        hash.0
+    }
+}
+
+/// Generates a 256-bit perceptual hash of an image.
 ///
 /// # Examples
 ///
 /// ```
-/// # #[cfg(all(feature = "image", feature = "std"))]
-/// # {
-/// use blockhash::blockhash144_string;
-///
-/// let img = image::open("tests/images/512x512_rgb.png").unwrap();
-/// let hash = blockhash144_string(&img);
-///
-/// assert_eq!(hash, "93fc0d913bd318332b37c37d308328e2ef83");
-/// # }
-/// ```
-#[cfg(feature = "std")]
-#[cfg_attr(docsrs, doc(cfg(feature = "std")))]
-#[inline(always)]
-pub fn blockhash144_string<I: Image>(img: &I) -> String {
-    impl144::blockhash_string(img)
-}
-
-/// Generates a 256-bit perceptual hash of an image as an array of bytes.
-///
-/// # Examples
-///
-/// ```
-/// # #[cfg(feature = "image")]
-/// # {
-/// use blockhash::blockhash256;
+/// # #[cfg(feature = "image")] {
+/// use blockhash::{blockhash256, Blockhash256};
 ///
 /// let img = image::open("tests/images/512x512_rgb.png").unwrap();
 /// let hash = blockhash256(&img);
 ///
 /// assert_eq!(
-///     &hash[..],
-///     &[
+///     hash,
+///     Blockhash256::from([
 ///         0x9c, 0xfd, 0xe0, 0x3d, 0xc4, 0x19, 0x84, 0x67,
 ///         0xad, 0x67, 0x1d, 0x17, 0x1c, 0x07, 0x1c, 0x5b,
 ///         0x1f, 0xf8, 0x1b, 0xf9, 0x19, 0xd9, 0x18, 0x18,
 ///         0x38, 0xf8, 0xf8, 0x90, 0xf8, 0x07, 0xff, 0x01,
-///     ],
+///     ]),
 /// );
 /// # }
 /// ```
 #[inline(always)]
-pub fn blockhash256<I: Image>(img: &I) -> [u8; 32] {
-    impl256::blockhash(img)
+pub fn blockhash256<I: Image>(img: &I) -> Blockhash256 {
+    Blockhash256(impl256::blockhash(img))
 }
 
-/// Generates a 256-bit perceptual hash of an image as a hex string.
-///
-/// # Examples
-///
-/// ```
-/// # #[cfg(all(feature = "image", feature = "std"))]
-/// # {
-/// use blockhash::blockhash256_string;
-///
-/// let img = image::open("tests/images/512x512_rgb.png").unwrap();
-/// let hash = blockhash256_string(&img);
-///
-/// assert_eq!(
-///     hash,
-///     "9cfde03dc4198467ad671d171c071c5b1ff81bf919d9181838f8f890f807ff01",
-/// );
-/// # }
-/// ```
-#[cfg(feature = "std")]
-#[cfg_attr(docsrs, doc(cfg(feature = "std")))]
-#[inline(always)]
-pub fn blockhash256_string<I: Image>(img: &I) -> String {
-    impl256::blockhash_string(img)
+/// A 256-bit hash digest.
+#[derive(Debug, Copy, Clone, Hash, PartialEq, Eq, PartialOrd, Ord)]
+pub struct Blockhash256([u8; 32]);
+
+impl Blockhash256 {
+    /// Returns the Hamming distance between two hashes.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use blockhash::Blockhash256;
+    ///
+    /// let a = Blockhash256::from([
+    ///     0xf0, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7,
+    ///     0xf8, 0xf9, 0xfa, 0xfb, 0xfc, 0xfd, 0xfe, 0xff,
+    ///     0xff, 0xef, 0xdf, 0xcf, 0xbf, 0xaf, 0x9f, 0x8f,
+    ///     0x7f, 0x6f, 0x5f, 0x4f, 0x3f, 0x2f, 0x1f, 0x0f,
+    /// ]);
+    /// let b = Blockhash256::from([
+    ///     0x00, 0xf1, 0xf2, 0xf3, 0xf4, 0xf5, 0xf6, 0xf7,
+    ///     0xf8, 0xf9, 0x3a, 0xfb, 0xfc, 0xfd, 0x0e, 0xff,
+    ///     0xff, 0xff, 0xdf, 0xcf, 0xbf, 0xaf, 0x9f, 0x8f,
+    ///     0x7f, 0x6f, 0x5f, 0x4f, 0x3f, 0x2f, 0x1f, 0x0f,
+    /// ]);
+    ///
+    /// assert_eq!(a.distance(&b), 11);
+    /// ```
+    #[inline(always)]
+    pub fn distance(&self, other: &Self) -> u32 {
+        impl256::distance(&self.0, &other.0)
+    }
+}
+
+impl FromStr for Blockhash256 {
+    type Err = BlockhashParseError;
+
+    #[inline(always)]
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        impl256::from_str(s).map(Self)
+    }
+}
+
+impl Display for Blockhash256 {
+    #[inline(always)]
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        impl256::fmt(f, self.0)
+    }
+}
+
+impl From<[u8; 32]> for Blockhash256 {
+    #[inline(always)]
+    fn from(bytes: [u8; 32]) -> Self {
+        Blockhash256(bytes)
+    }
+}
+
+impl From<Blockhash256> for [u8; 32] {
+    #[inline(always)]
+    fn from(hash: Blockhash256) -> Self {
+        hash.0
+    }
 }
